a35e249750a13fa2a04ad8f43030beb314c3c707
Noob0799 <uchihaitachi0799@gmail.com>
2020-04-28 23:32:38 +0530

    Delivery component basic functionalities completed

--- backend/routes/delivery.js
+++ backend/routes/delivery.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
 const DeliveryList = require('../models/productForDelivery');
 
 router.post('/add', (req,res,next) => {
@@ -16,6 +17,19 @@ router.get('/get', (req,res,next) => {
         .then(result => {
             res.status(201).json({message: "Products bought successfully!!", data: result});
         })
+})
+
+router.get('/get/deliverymen', (req,res,next) => {
+    const list = ["John", "Jack"];
+    res.status(201).json({message: "Deliverymen list received successfully!!", data: list});
+})
+
+router.put('/update', (req,res,next) => {
+    console.log('Delivery Id to be updated:', req.body);
+    DeliveryList.updateOne({'_id': ObjectId(req.body._id)}, {deliveryman: req.body.deliveryman, delivered: "Yes"}).then(result => {
+        console.log(result._id);
+        res.status(201).json({message: "Delivery details updated successfully!!"});
+      });
 })
 
 module.exports = router;
