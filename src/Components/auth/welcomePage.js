09d240f76907b7a8a45825057969849847741126
Noob0799 <uchihaitachi0799@gmail.com>
2020-05-30 21:39:58 +0530

    Incorporated changes for welcomne page

--- src/Components/auth/welcomePage.js
+++ src/Components/auth/welcomePage.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./welcomePage.css";
+import bg from "./bg.png";
 function welcomePage() {
   return (
-    <div className="welcome-page">
-      <div className="welcome-head">
-        <h3>Welcome to FlexMart</h3>
+    <div class="x">
+      <div class="bg">
+        <div class="bg bg2"></div>
+        <div class="bg bg3">
+          <img src={bg} alt="banner" />
+        </div>
       </div>
-      <div className="welcome-components">
-        <Link className="welcome-x" to="/SignUp/Customer">
-        <button type="button" className="btn btn-dark">Sign Up</button>
-        </Link>
-        <Link className="welcome-x" to="/Login">
-        <button type="button" className="btn btn-dark">Login</button>
-        </Link>
+
+      <div className="welcome-page">
+        <div className="welcome-head">
+          <h3>FLEXMART</h3>
+        </div>
+        <div className="welcome-components">
+          <Link className="welcome-x" to="/SignUp/Customer">
+            <button type="button" className="btn btn-light">
+              Sign Up
+            </button>
+          </Link>
+          <Link className="welcome-x" to="/Login">
+            <button type="button" className="btn btn-light">
+              Login
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
